04b6252cafbe2eb8805c230cc1e86b6976858c5c
BluMotiv-sushen <sushen@blumotiv.com>
2026-08-09 16:04:26 +0530

    fixed new tab last scroll issue and tab-refresh issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,12 @@
 // App.jsx
+import { useEffect } from "react";
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import Certifications from "./pages/Certifications";
 import Journey from "./pages/Journey";
 import Algorithms from "./pages/Algorithms";
@@ -10,9 +16,20 @@ import Footer from "./components/Footer";
 import ContactFab from "./components/ContactFab";
 import Experience from "./pages/Experience";
 
+function ScrollToTop() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+  }, [location.pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path="/certifications" element={<Certifications />} />
